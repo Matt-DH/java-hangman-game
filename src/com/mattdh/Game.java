58db3a2837d00349b8019d444a6bef7c8caca633
Matt-DH <mattdavidhardy@gmail.com>
2022-08-11 15:20:57 -0230

    Commit

--- src/com/mattdh/Game.java
+++ src/com/mattdh/Game.java
@@ -4,45 +4,59 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Game {
+public class Game implements Visuals {
 
     // Objects
     Scanner sc = new Scanner(System.in);
 
     // Variables
-    private int tries;
+    private int tries = 7;
+    private char[] nameCharArray;
     private String name;
     private String game;
     private String guess;
+    private char[] answerCharArray;
 
     public void setupWord(ArrayList<String[]> namesAndGames) {
         int randInt = ThreadLocalRandom.current().nextInt(0, namesAndGames.size());
-        name = namesAndGames.get(randInt)[0];
-        System.out.println(name);
+        nameCharArray = namesAndGames.get(randInt)[0].toCharArray();
         game = namesAndGames.get(randInt)[1];
     }
 
     public void startGame() {
-        tries = 10;
-        System.out.println();
-        System.out.println("------------------------------");
-        System.out.println("----------GAME START----------");
-        System.out.println("------------------------------");
-        System.out.println();
 
+        for (char c : nameCharArray) {
+            name += c;
+        }
+
+        System.out.println(name);
+
+        answerCharArray = name.replaceAll("[A-Za-z]", "#").toCharArray();
+
+        System.out.println(gameStartMessage);
         while (tries > 0) {
+            System.out.println(hangManVisuals[tries - 1]);
+            System.out.print("NAME:   ");
+
+            for (char c : answerCharArray) {
+                System.out.print(String.valueOf(c).toUpperCase());
+            }
+
+            System.out.println();
             System.out.println("REMAINING TRIES: " + tries);
             System.out.println();
 
+            System.out.print("GUESS:   ");
             guess = sc.next();
 
-            if (guess.matches(name)) {
-                System.out.println("WIN");
-                break;
-            } else {
-                System.out.println("WRONG");
-                tries -= 1;
+            while (name.contains(guess)) {
+                answerCharArray[name.indexOf(guess)] = name.charAt(name.indexOf(guess));
+                nameCharArray[name.indexOf(guess)] = '#';
             }
+
+            System.out.println();
+            System.out.println("------------------------------");
+            System.out.println();
         }
 
         if (tries == 0) {
--- src/com/mattdh/Visuals.java
+++ src/com/mattdh/Visuals.java
@@ -1,4 +1,58 @@
 package com.mattdh;
 
-public class Visuals {
+import java.util.ArrayList;
+
+public interface Visuals {
+
+    String gameStartMessage =
+            "\n" +
+            "------------------------------\n" +
+            "----------GAME START----------\n" +
+            "------------------------------\n";
+    String hangMan1 =
+            "\n" +
+                    "  o  \n" +
+                    " /|\\ \n" +
+                    "  |  \n" +
+                    " / \\ \n";
+    String hangMan2 =
+            "\n" +
+                    "  o  \n" +
+                    " /|\\ \n" +
+                    "  |  \n" +
+                    " /\n";
+    String hangMan3 =
+            "\n" +
+                    "  o  \n" +
+                    " /|\\ \n" +
+                    "  |  \n" +
+                    "\n";
+    String hangMan4 =
+            "\n" +
+                    "  o  \n" +
+                    " /|\n" +
+                    "  |  \n" +
+                    "\n";
+    String hangMan5 =
+            "\n" +
+                    "  o  \n" +
+                    "  |\n" +
+                    "  |  \n" +
+                    "\n";
+    String hangMan6 =
+            "\n" +
+                    "  o  \n" +
+                    "  |\n" +
+                    "\n" +
+                    "\n";
+    String hangMan7 =
+            "\n" +
+                    "  o  \n" +
+                    "\n" +
+                    "\n" +
+                    "\n";
+
+    String[] hangManVisuals = new String[] {
+            hangMan7, hangMan6, hangMan5, hangMan4, hangMan3, hangMan2, hangMan1
+    };
 }
